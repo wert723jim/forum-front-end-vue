130bbfd95b2b50ae7bfe013cfabc268983599492
wert723jim <wert723jim@gmail.com>
2022-11-02 16:52:59 +0800

    Adding component Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="restaurant-forum">
-    <router-view/>
+    <Navbar />
+    <main
+      class="mt-5 bg-white"
+    >
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
